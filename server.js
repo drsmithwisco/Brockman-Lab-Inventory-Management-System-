fef778623597e8cd4e107e6901467696c4f21358
drsmithwisco <119891474+drsmithwisco@users.noreply.github.com>
2025-03-04 23:24:45 -0600

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve static files from the public folder (make sure your index.html is in a folder called "public")
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 // Connect to MongoDB using your cluster connection string
 mongoose.connect(
